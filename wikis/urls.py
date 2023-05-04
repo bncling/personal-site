7e53c4f81326431f09afa599e14a0aeea8329cbe
Ben Clingenpeel <benclingenpeel@gmail.com>
2023-05-04 11:38:56 -0400

    adding about page, bird map, and CV

--- wikis/urls.py
+++ wikis/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.homepage, name = "homepage")
+	path("", views.homepage, name = "homepage"),
+	path("about/", views.about, name = "about"),
+	path("birds/", views.birds, name = "birds"),
 ]
--- wikis/views.py
+++ wikis/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
 	return render(request, "wikis/home.html")
+
+def about(request):
+	return render(request, "wikis/about.html", {"title": "About"})
+
+def birds(request):
+	return render(request, "wikis/birdMap.html", {"title": "Birds"})
