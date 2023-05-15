e2d216dbb1f33ecf6074dbdd516dcdbb457bbbcb
Ben Clingenpeel <benclingenpeel@gmail.com>
2023-05-15 15:47:28 -0400

    simple JS animation

--- wikis/static/wikis/test.js
+++ wikis/static/wikis/test.js
@@ -1,1 +1,20 @@
-alert("hi there");
+var originalLocation = 20;
+
+var circleInterval;
+
+function circleAnimation(time) {
+
+	originalLocation++;
+	document.getElementById("circle").style.left = originalLocation + "px";
+
+	if (time < 4000) {
+		circleInterval = requestAnimationFrame(circleAnimation);
+	} 
+
+	else {
+		document.getElementById("circle").style.backgroundColor = "dimgrey";
+		cancelAnimationFrame(circleInterval);
+	}
+}
+
+circleInterval = requestAnimationFrame(circleAnimation);
